9d9767d9ccb6369b5fd321567fb713ffbd8d5d58
Alessandro Talamona <info@talale.it>
2026-03-15 14:56:28 +0100

    reset password email via brevo

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,9 +3,12 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { admin } from "better-auth/plugins";
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { BrevoClient } from "@getbrevo/brevo";
 
 const adapter = new PrismaPg({ connectionString: import.meta.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
+
+const brevo = new BrevoClient({ apiKey: import.meta.env.BREVO_API_KEY });
 
 export const auth = betterAuth({
     baseURL: import.meta.env.BETTER_AUTH_URL,
@@ -15,7 +18,11 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
         sendResetPassword: async ({ user, url }) => {
-            console.log(url);
+            await brevo.transactionalEmails.sendTransacEmail({
+                templateId: 7,
+                to: [{ email: user.email, name: user.name }],
+                params: { reset_link: url },
+            });
         },
     },
     plugins: [
